e14f08253e1199e4fbc3a9ce526a80fd535b90c4
Sharon <sharon@workplacearcade.com>
2018-03-24 20:06:13 +1100

    First commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Welcome to Envylope</h1>
+        </header>
+        <p className="App-intro">
+          Calligraphy and stationery
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
